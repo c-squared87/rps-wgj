52c527239fd1594dfd70526b2238c1db6b5947aa
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-06-21 21:17:58 -0500

    built DetectsPlayer and EventsManager classes

--- Week 154 - RPS/Assets/Scripts/DetectsPlayer.cs
+++ Week 154 - RPS/Assets/Scripts/DetectsPlayer.cs
@@ -5,6 +5,6 @@ public class DetectsPlayer : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
-            Debug.Log("Hit player!");
+            EventsManager.EndGame();
     }
 }
---
+++ Week 154 - RPS/Assets/Scripts/EventsManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+public static class EventsManager
+{
+
+    /*
+    INDEX
+
+    OnGameEnd
+    OnMessageBroadcast
+    OnScoreChanged
+    OnLevelStart
+
+    */
+
+    #region EndGame
+
+    public delegate void OnGameEnd();
+    static event OnGameEnd onGameEnd;
+
+    public static void ADD_GameEndListener(EventsManager.OnGameEnd _method)
+    {
+        onGameEnd += _method;
+    }
+    public static void REMOVE_GameEndListener(EventsManager.OnGameEnd _method)
+    {
+        onGameEnd -= _method;
+    }
+
+    public static void EndGame()
+    {
+        if (onGameEnd != null)
+            onGameEnd();
+    }
+
+    #endregion
+
+    #region BroadcastMessage
+
+    public delegate void OnMessageBroadcast(string _message);
+    static event OnMessageBroadcast onMessageBroadcast;
+
+    public static void ADD_OnMessageBroadcastListener(EventsManager.OnMessageBroadcast _method)
+    {
+        onMessageBroadcast += _method;
+    }
+    public static void REMOVE_OnMessageBroadcastListener(EventsManager.OnMessageBroadcast _method)
+    {
+        onMessageBroadcast -= _method;
+    }
+
+    public static void BroadcastMessage(string _message)
+    {
+        if (onMessageBroadcast != null)
+            onMessageBroadcast(_message);
+    }
+
+    #endregion
+
+    #region BroadcastMessage
+
+    public delegate void OnScoreChanged(int _value);
+    static event OnScoreChanged onScoreChanged;
+
+    public static void ADD_OnScoreChangedListener(EventsManager.OnScoreChanged _method)
+    {
+        onScoreChanged += _method;
+    }
+    public static void REMOVE_OnScoreChangedListener(EventsManager.OnScoreChanged _method)
+    {
+        onScoreChanged -= _method;
+    }
+
+    public static void ScoreChanged(int _value)
+    {
+        if (onScoreChanged != null)
+            onScoreChanged(_value);
+    }
+
+    #endregion
+
+    #region LevelStart
+
+    public delegate void OnLevelStart();
+    static event OnLevelStart onLevelStart;
+
+    public static void ADD_OnLevelStartListener(EventsManager.OnLevelStart _method)
+    {
+        onLevelStart += _method;
+    }
+    public static void REMOVE_OnLevelStartListener(EventsManager.OnLevelStart _method)
+    {
+        onLevelStart -= _method;
+    }
+
+    public static void LevelStart()
+    {
+        if (onLevelStart != null)
+            onLevelStart();
+    }
+
+    #endregion
+}
