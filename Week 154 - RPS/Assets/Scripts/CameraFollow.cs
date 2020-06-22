05984b25f09acf399b0f15d2b48bcbccad82d0d1
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-06-21 19:38:52 -0500

    player movement implemented and camera follow class and functionality built

---
+++ Week 154 - RPS/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float x_offset;
+    public float y_offset;
+
+
+    void LateUpdate()
+    {
+        Vector3 _newPos = target.position;
+
+        _newPos.x += x_offset;
+        _newPos.y += y_offset;
+        _newPos.z = -10;
+
+        transform.position = _newPos;
+    }
+}
---
+++ Week 154 - RPS/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float jumpHeight;
+    [SerializeField] float moveSpeed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.fixedDeltaTime);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (rb.velocity.y <= 0)
+        {
+            rb.gravityScale = 2;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpHeight * Time.fixedDeltaTime, ForceMode2D.Impulse);
+    }
+}
