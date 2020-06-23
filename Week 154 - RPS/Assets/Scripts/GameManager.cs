5de42b3ccd2549afdee89159258c619b83dfe9e7
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-06-23 10:15:02 -0500

    more game manager functions added

--- Week 154 - RPS/Assets/Scripts/GameManager.cs
+++ Week 154 - RPS/Assets/Scripts/GameManager.cs
@@ -6,21 +6,51 @@ public class GameManager : MonoBehaviour
 {
     public float SpeedMultiplier = 3.5f;
 
+    GameObject[] activeOnGameOver;
+
     void Start()
     {
-        Time.timeScale = 1;
-        EventsManager.LevelStart();
-        StartCoroutine("LevelUp");        
+        InitGame();
     }
 
+    void InitGame()
+    {
+        Time.timeScale = 1;
 
+        activeOnGameOver = GameObject.FindGameObjectsWithTag("ActiveOnGameOver");
+
+        foreach (GameObject _object in activeOnGameOver)
+        {
+            _object.SetActive(false);
+        }
+
+        EventsManager.LevelStart();
+
+        StartCoroutine("LevelUp");
+    }
+
+    void EndGame()
+    {
+        Time.timeScale = 0;
+
+        foreach (GameObject _object in activeOnGameOver)
+        {
+            _object.SetActive(true);
+        }
+    }
+
+    // CALLED FROM ELSEWHERES.
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
 
     IEnumerator LevelUp()
     {
         while (true)
         {
             yield return new WaitForSeconds(14);
-            Time.timeScale += 0.2f;
+            Time.timeScale += 0.1f;
         }
     }
 }
